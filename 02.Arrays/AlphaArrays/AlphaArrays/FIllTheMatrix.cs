7f10e31968550e94937de1be8fa9378328d79f3e
Plamen Atanasov <plamen.atanasov91@gmail.com>
2017-11-24 13:38:56 +0200

    add 1 and 4 array tasks

---
+++ 02.Arrays/AlphaArrays/AlphaArrays/FIllTheMatrix.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaArrays
+{
+    class FIllTheMatrix
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int ch = Console.Read();
+
+            switch (ch)
+            {
+                case 'a':
+                    FillMatrixCaseA(n);
+                    break;
+                case 'b':
+                    FillMatrixCaseB(n);
+                    break;
+                case 'c':
+                    FillMatrixCaseC(n);
+                    break;
+                case 'd':
+                    FillMatrixCaseD(n);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void FillMatrixCaseA(int n)
+        {
+            int[,] matrix = new int[n, n];
+            int counter = 1;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[col, row] = counter;
+                    counter++;
+                }
+            }
+
+            DisplayMatrix(matrix);
+        }
+
+        static void FillMatrixCaseB(int n)
+        {
+            int[,] matrix = new int[n, n];
+            int counter = 1;
+
+            for (int col = 0; col < n; col++)
+            {
+                if (col % 2 == 0)
+                {
+                    for (int row = 0; row < n; row++)
+                    {
+                        matrix[row, col] = counter;
+                        counter++;
+                    }
+                }
+                else
+                {
+                    for (int row = n - 1; 0 <= row; row--)
+                    {
+                        matrix[row, col] = counter;
+                        counter++;
+                    }
+                }
+            }
+
+            DisplayMatrix(matrix);
+        }
+
+        static void FillMatrixCaseC(int n)
+        {
+            int[,] matrix = new int[n, n];
+            int counter = 1;
+            int col = 0;
+            int row = n - 1;
+            int edge = n - 1;
+
+            while (counter <= n * n)
+            {
+                matrix[row, col] = counter;
+                counter++;
+
+                if (row + 1 > edge && col + 1 <= edge)
+                {
+                    row = (n - 1) - (col + 1);
+                    col = 0;
+                }
+                else if ((row + 1 > edge || row + 1 <= edge) && col + 1 > edge)
+                {
+                    col = (n - 1 - row) + 1;
+                    row = 0;
+                }
+                else
+                {
+                    row++;
+                    col++;
+                }
+            }
+
+            DisplayMatrix(matrix);
+        }
+
+        static void FillMatrixCaseD(int n)
+        {
+            int[,] matrix = new int[n, n];
+            int counter = 1;
+            int rotations = n * n;
+            string dir = "down";
+            int edge = n - 1;
+            int row = 0;
+            int col = 0;
+
+            while(counter <= rotations)
+            {
+                if (dir == "down" && row <= edge && matrix[row, col] == 0)
+                {
+                    matrix[row, col] = counter;
+                    counter++;
+                    row++;
+                }
+                else if (dir == "down")
+                {
+                    row--;
+                    col++;
+                    dir = "right";
+                }
+
+                if (dir == "right" && col <= edge && matrix[row, col] == 0)
+                {
+                    matrix[row, col] = counter;
+                    counter++;
+                    col++;
+                }
+                else if (dir == "right")
+                {
+                    col--;
+                    row--;
+                    dir = "up";
+                }
+
+                if (dir == "up" && row >= 0 && matrix[row, col] == 0)
+                {
+                    matrix[row, col] = counter;
+                    counter++;
+                    row--;
+                }
+                else if (dir == "up")
+                {
+                    row++;
+                    col--;
+                    dir = "left";
+                }
+
+                if (dir == "left" && col >= 0 && matrix[row, col] == 0)
+                {
+                    matrix[row, col] = counter;
+                    counter++;
+                    col--;
+                }
+                else if (dir == "left")
+                {
+                    col++;
+                    row++;
+                    dir = "down";
+                }
+            }
+
+            DisplayMatrix(matrix);
+        }
+
+        static void DisplayMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == matrix.GetLength(1)-1)
+                    {
+                        Console.Write(matrix[i, j]);
+                        continue;
+                    }
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 02.Arrays/AlphaArrays/BishopPathFinder/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BishopPathFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int[,] field = new int[rows, cols];
+
+            long result = 0;
+            int row = field.GetLength(0) - 1;
+            int col = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+                string direction = command[0];
+                int moves = int.Parse(command[1]);
+
+                if (direction == "RU" || direction == "UR")
+                {
+                    for (int j = 0; j < moves; j++)
+                    {
+                        if (row < 0 || col > cols - 1)
+                        {
+                            row++;
+                            col--;
+                            break;
+                        }
+
+                        if (field[row, col] == 0)
+                        {
+                            result += (col + (rows - row - 1)) * 3;
+                            field[row, col] = -1;
+                        }
+
+                        if (j != moves - 1)
+                        {
+                            row--;
+                            col++;
+                        }
+
+                        
+                    }
+                }
+
+                if (direction == "LU" || direction == "UL")
+                {
+                    for (int j = 0; j < moves; j++)
+                    {
+                        if (row < 0 || col < 0)
+                        {
+                            row++;
+                            col++;
+                            break;
+                        }
+
+                        if (field[row, col] == 0)
+                        {
+                            result += (col + (rows - row - 1)) * 3;
+                            field[row, col] = -1;
+                        }
+
+                        if (j != moves - 1)
+                        {
+                            row--;
+                            col--;
+                        }
+                    }
+                }
+
+                if (direction == "LD" || direction == "DL")
+                {
+                    for (int j = 0; j < moves; j++)
+                    {
+                        if (row > rows - 1 || col < 0)
+                        {
+                            row--;
+                            col++;
+                            break;
+                        }
+
+                        if (field[row, col] == 0)
+                        {
+                            result += (col + (rows - row - 1)) * 3; 
+                            field[row, col] = -1;
+                        }
+
+                        if (j != moves - 1)
+                        {
+                            row++;
+                            col--;
+                        }
+                    }
+                }
+
+                if (direction == "RD" || direction == "DR")
+                {
+                    for (int j = 0; j < moves; j++)
+                    {
+                        if (row > rows - 1 || col > cols - 1)
+                        {
+                            row--;
+                            col--;
+                            break;
+                        }
+
+                        if (field[row, col] == 0)
+                        {
+                            result += (col + (rows - row - 1)) * 3;
+                            field[row, col] = -1;
+                        }
+
+                        if (j != moves - 1)
+                        {
+                            row++;
+                            col++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == matrix.GetLength(1) - 1)
+                    {
+                        Console.Write(matrix[i, j]);
+                        continue;
+                    }
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
