d0ad4a47d9017ab9bbc0d4ce6780357946ffae87
Plamen Atanasov <plamen.atanasov91@gmail.com>
2018-01-20 17:03:32 +0200

    adding player ranking and units of work solutions

---
+++ 05. DSA/01. Linear Data Structures/AlphaLinearDataStructures/PlayerRanking/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace PlayerRanking
+{
+    class Program
+    {
+        private static BigList<Player> ranklist = new BigList<Player>();
+        private static Dictionary<string, OrderedSet<Player>> sortedTypes = new Dictionary<string, OrderedSet<Player>>();
+        private static StringBuilder resultBuilder = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+                switch (input[0])
+                {
+                    case "add":
+                        string name = input[1];
+                        string type = input[2];
+                        int age = int.Parse(input[3]);
+                        int position = int.Parse(input[4]);
+
+                        Add(name, type, age, position);
+
+                        resultBuilder.AppendFormat($"Added player {name} to position {position}");
+                        resultBuilder.AppendLine();
+                        break;
+                    case "find":
+                        string findType = input[1];
+                        resultBuilder.Append(Find(findType));
+                        resultBuilder.AppendLine();
+                        break;
+                    case "ranklist":
+                        int start = int.Parse(input[1]);
+                        int end = int.Parse(input[2]);
+                        resultBuilder.Append(GetRanklist(start, end));
+                        resultBuilder.AppendLine();
+                        break;
+                    default:
+                        Console.WriteLine(resultBuilder.ToString().Trim());
+                        return;
+                }
+            }
+        }
+
+        static void Add(string name, string type, int age, int position)
+        {
+            var player = new Player(name, type, age);
+            ranklist.Insert(position - 1, player);
+
+            if (!sortedTypes.ContainsKey(type))
+            {
+                sortedTypes.Add(type, new OrderedSet<Player>());
+            }
+
+            if (sortedTypes[type].Count == 5)
+            {
+                Player lastPlayer = sortedTypes[type][4];
+                if (lastPlayer.CompareTo(player) > 0)
+                {
+                    sortedTypes[type].Remove(lastPlayer);
+                    sortedTypes[type].Add(player);
+                }
+            }
+            else
+            {
+                sortedTypes[type].Add(player);
+            }
+        }
+
+        static string Find(string type)
+        {
+            var sb = new StringBuilder();
+            sb.Append($"Type {type}: ");
+
+           
+            if (sortedTypes.ContainsKey(type))
+            {
+                //for (int i = 0; i < sortedTypes[type].Count; i++)
+                //{
+                //    sb.Append($"{sortedTypes[type].ElementAt(i)} ");
+                //}
+                var players = sortedTypes[type];
+                foreach (Player player in players)
+                {
+                    sb.AppendFormat(" {0}", player);
+                }
+
+                return sb.ToString().TrimEnd(new char[] { ' ', ';' });
+            }
+            else
+            {
+                return sb.ToString();
+            }
+        }
+
+        static string GetRanklist(int start, int end)
+        {
+            var sb = new StringBuilder();
+
+            //for (int i = start - 1, j = 1; i < end; i++, j++)
+            //{
+            //    sb.AppendFormat(string.Format("{0}. {1} ", i + 1, ranklist[i]));
+            //}
+
+            int count = end - start + 1;
+            var rankedPlayers = ranklist.Range(start - 1, count);
+
+            int playerPosition = start;
+            foreach (Player player in rankedPlayers)
+            {
+                sb.AppendFormat("{0}. {1} ", playerPosition++, player);
+            }
+
+            return sb.ToString().TrimEnd(new char[] { ' ', ';' });
+        }
+    }
+
+    class Player : IComparable<Player>
+    {
+        private string name;
+        private string type;
+        private int age;
+
+        public Player(string name, string type, int age)
+        {
+            this.name = name;
+            this.type = type;
+            this.age = age;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if (this.name.CompareTo(other.name) == 0)
+            {
+                return other.age.CompareTo(this.age);
+            }
+
+            return this.name.CompareTo(other.name);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}({1});", this.name, this.age);
+        }
+    }
+}
---
+++ 05. DSA/01. Linear Data Structures/AlphaLinearDataStructures/UnitsOfWork/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace UnitsOfWork
+{
+    class Program
+    {
+        private static Dictionary<string, SortedSet<Unit>> unitsByType = new Dictionary<string, SortedSet<Unit>>();
+        private static Dictionary<string, Unit> unitsByName = new Dictionary<string, Unit>();
+        private static OrderedSet<Unit> unitsByPower = new OrderedSet<Unit>();
+        private static StringBuilder result = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] inputParams = input.Split(' ');
+                switch (inputParams[0])
+                {
+                    case "add":
+                        AddUnit(inputParams[1], inputParams[2], int.Parse(inputParams[3]));
+                        break;
+                    case "remove":
+                        RemoveUnit(inputParams[1]);
+                        break;
+                    case "find":
+                        FindUnit(inputParams[1]);
+                        break;
+                    case "power":
+                        GetPowerUnits(int.Parse(inputParams[1]));
+                        break;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static void AddUnit(string name, string type, int attack)
+        {
+            if (unitsByName.ContainsKey(name))
+            {
+                result.AppendLine(string.Format("FAIL: {0} already exists!", name));
+                return;
+            }
+
+            var unit = new Unit(name, type, attack);
+
+            if (!unitsByType.ContainsKey(type))
+            {
+                unitsByType.Add(type, new SortedSet<Unit>());
+            }
+
+            unitsByName.Add(name, unit);
+            unitsByType[type].Add(unit);
+            unitsByPower.Add(unit);
+
+            result.AppendLine(string.Format("SUCCESS: {0} added!", name));
+        }
+
+        static void RemoveUnit(string name)
+        {
+            if (!unitsByName.ContainsKey(name))
+            {
+                result.AppendFormat("FAIL: {0} could not be found!", name);
+                result.AppendLine();
+                return;
+            }
+
+            var unit = unitsByName[name];
+
+            unitsByName.Remove(name);
+            unitsByType[unit.Type].Remove(unit);
+            unitsByPower.Remove(unit);
+            result.AppendFormat("SUCCESS: {0} removed!", name);
+            result.AppendLine();
+        }
+
+        static void FindUnit(string type)
+        {
+            result.Append("RESULT: ");
+            if (!unitsByType.ContainsKey(type) || !unitsByType[type].Any())
+            {
+                result.AppendLine();
+                return;
+            }
+
+            var units = unitsByType[type].Take(10);
+
+            foreach (var unit in units)
+            {
+                result.AppendFormat("{0}, ", unit);
+            }
+
+            result.Remove(result.Length - 2, 2);
+            result.AppendLine();
+        }
+
+        static void GetPowerUnits(int count)
+        {
+            IEnumerable<Unit> range = unitsByPower.Take(count);
+            result.Append("RESULT: ");
+
+            foreach (var unit in range)
+            {
+                result.AppendFormat("{0}, ", unit);
+            }
+
+            if (unitsByPower.Any())
+            {
+                result.Remove(result.Length - 2, 2);
+            }
+
+            result.AppendLine();
+        }
+    }
+
+    class Unit : IComparable<Unit>
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Attack { get; set; }
+
+        public Unit(string name, string type, int attack)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Attack = attack;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name}[{this.Type}]({this.Attack})";
+        }
+
+        public int CompareTo(Unit other)
+        {
+            int compare = other.Attack.CompareTo(this.Attack);
+
+            if (compare == 0)
+            {
+                compare = this.Name.CompareTo(other.Name);
+            }
+
+            return compare;
+        }
+    }
+}
