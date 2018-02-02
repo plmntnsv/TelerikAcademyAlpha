c347795b9970bebd0bb0b5a6f8960262125d44af
Plamen Atanasov <plamen.atanasov91@gmail.com>
2018-02-02 19:17:01 +0200

    more tasks

---
+++ 05. DSA/PathToOne/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathToOne
+{
+    class Program
+    {
+        private static int bestCount = int.MaxValue;
+        private static int startNum;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            startNum = n;
+
+            CalculatePathToOne(n, 0, true, true);
+
+            Console.WriteLine(bestCount);
+        }
+
+        private static void CalculatePathToOne(int n, int count, bool add, bool subtract)
+        {
+            if (count > bestCount)
+            {
+                return;
+            }
+
+            if (n < 1)
+            {
+                return;
+            }
+
+            if (n > startNum + 1)
+            {
+                return;
+            }
+
+            if (n == 1)
+            {
+                if (bestCount > count)
+                {
+                    bestCount = count;
+                }
+
+                return;
+            }
+
+            if (n % 2 == 0)
+            {
+                CalculatePathToOne(n / 2, count + 1, true, true);
+            }
+            else
+            {
+                if (add)
+                {
+                    CalculatePathToOne(n + 1, count + 1, true, false);
+                }
+
+                if (subtract)
+                {
+                    CalculatePathToOne(n - 1, count + 1, false, true);
+                }
+            }
+        }
+    }
+}
---
+++ 05. DSA/PlusOneMultipleOne/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusOneMultipleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = input[0];
+            int m = input[1];
+
+            var queue = new Queue<int>();
+
+            queue.Enqueue(n);
+
+            int current = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                current = queue.Dequeue();
+                
+                queue.Enqueue(current + 1);
+                queue.Enqueue(2 * current + 1);
+                queue.Enqueue(current + 2);
+            }
+
+            Console.WriteLine(current);
+        }
+    }
+}
